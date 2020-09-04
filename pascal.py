3b06b560745d1450ece7898bbc9e752f828dd1ea
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2020-09-04 16:41:04 +1200

    Add comments to code

--- pascal.py
+++ pascal.py
@@ -2,6 +2,8 @@ import pprint
 
 pp = pprint.PrettyPrinter()
 
+#  Calculates next row of Pascal's triangle based on previous row
+
 
 def calculate_next_row(curr_row):
     if curr_row == []:
@@ -12,12 +14,17 @@ def calculate_next_row(curr_row):
     next_row.append(1)
     return next_row
 
+# Generatest Pascal's triangle up to n rows
+
 
 def pascal_gen(n):
     pascal_triangle = [[1]]
     while len(pascal_triangle[-1]) < n:
         pascal_triangle.append(calculate_next_row(pascal_triangle[-1]))
     return pascal_triangle
+
+# Makes a table (python dictionary) of counts for each number found in
+# Pascal's triangle
 
 
 def count_number_frequencies(triangle):
@@ -36,14 +43,15 @@ def count_number_frequencies(triangle):
     return frequency_table
 
 
+# User input
 n = int(input("How many rows? "))
 min_freq = int(input("Minimum frequency? "))
 
-triangle = pascal_gen(n)
+pascals_triangle = pascal_gen(n)
 # print(triangle)
-counts = count_number_frequencies(triangle)
+counts = count_number_frequencies(pascals_triangle)
 
-threshold_counts = {}
+threshold_counts = {}  # Table of counts above the minimum requirement
 
 for (num, count) in counts.items():
     new_count = count
@@ -51,7 +59,5 @@ for (num, count) in counts.items():
         new_count += 2  # It'll appear 2 more times in its own row
     if new_count >= min_freq:
         threshold_counts[num] = new_count
-
-# print(counts)
 
 pp.pprint(threshold_counts)
